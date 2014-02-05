feae4aba9dedf3ad09eadcf73857ed1eacd73d4e
Peter Casinelli <pcas00@gmail.com>
2014-02-05 01:23:07 -0500

    Web scraping URL with pattern
    First iteration that uses a regex pattern to scrape a stock URL for a
    stock price.

--- src/com/example/android_web_scraping/WebScrapingActivity.java
+++ src/com/example/android_web_scraping/WebScrapingActivity.java
@@ -1,15 +1,24 @@
 package com.example.android_web_scraping;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
 public class WebScrapingActivity extends Activity {
-	//private String stockPattern2 = "Last\:<span>\&nbsp\;(\d+\.\d+)";
-	private String stockPattern = "(Last\\:\\<span\\>\\&nbsp\\;)(\\d+\\.\\d+)";
+	private String stockStringPattern = "(Last\\:\\<span\\>\\&nbsp\\;)(\\d+\\.\\d+)";
+	private Pattern stockPattern = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +35,67 @@ public class WebScrapingActivity extends Activity {
 	
 	public void updateStocks(View v) {
 		
-		Pattern p = Pattern.compile(stockPattern);
+		// check connectivity
+	    ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+        	Log.d(this.getClass().toString(), "Connected to the internet.");
+        	startThread("http://quotes.esignal.com/esignalprod/quote.action?s=AAPL", "AAPL");
+        } else {
+        	Log.e(this.getClass().toString(), "Could not get network connection.");
+        	//TODO Set field as unknown
+        }
+				
 		
 	}
+
+	private void startThread(final String site, final String stock) {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				final String stockPrice = scrapeStockPrice(site + stock);
+				//TODO set textfield with stock price
+			}		
+		}).start();		
+	}
+		
+	private String scrapeStockPrice(String site) {
+		BufferedReader in = null;
+		if (stockPattern == null)
+			stockPattern = Pattern.compile(stockStringPattern); // slow, only do once, and not on UI thread*/
+		try {
+			URL url = new URL(site);
+			in = new BufferedReader(
+			            new InputStreamReader(
+			            url.openStream()));
+			String inputLine;
+			while ((inputLine = in.readLine()) != null) {
+				Matcher m = stockPattern.matcher(inputLine);
+				if (m.find()) {
+					String s = m.group(2);
+					Log.d(this.getClass().toString(), "Group 2 is: " + s);
+					return s;
+				}
+			}
+			Log.d(this.getClass().toString(), "Done searching");
+
+		} catch (IOException e) {
+			Log.e(this.getClass().toString(), "Unable to open url: " /* + site */);
+		} catch (Exception e) {
+			Log.e(this.getClass().toString(), e.toString());
+		} finally {
+			if (in != null)
+				try {
+					in.close();
+				} catch (IOException e) {
+					// ignore, we tried and failed to close, limp along anyway
+				}
+		}
+		return "Unable to find stock";
+		
+	}
+	
+  
 
 }
